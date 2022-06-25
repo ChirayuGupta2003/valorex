fbea164064216bfa919820c25aff8e2258a6fca2
Mukul-Jindal <mukul20jindal58@gmail.com>
2022-06-25 14:53:57 +0530

    MODIFIED AGENTS PAGE

--- lib/Mukul/main.dart
+++ lib/Mukul/main.dart
@@ -1,3 +1,80 @@
-//Hello Guys.....
+import 'package:flutter/material.dart';
 
-//Hello
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("AGENTS",
+              style: TextStyle(
+                fontFamily: "Valorant1",
+                fontSize: 28,
+              )),
+          centerTitle: true,
+          toolbarHeight: 120.0,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        child: Image(
+                          image: NetworkImage(
+                              'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt26fcf1b5752514ee/5eb7cdbfc1dc88298d5d3799/V_AGENTS_587x900_Brimstone.png'),
+                        ),
+                        height: 175,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Text(
+                          "BRIMSTONE",
+                          style: TextStyle(
+                            fontFamily: 'Valorant1',
+                            fontSize: 20,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        child: Image(
+                          image: NetworkImage(
+                              'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltf0200e1821b5b39f/5eb7cdc144bf8261a04d87f9/V_AGENTS_587x900_Phx.png'),
+                        ),
+                        height: 175,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Text(
+                          "PHOENIX",
+                          style: TextStyle(
+                            fontFamily: 'Valorant1',
+                            fontSize: 20,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
