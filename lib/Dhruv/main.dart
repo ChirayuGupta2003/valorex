186400a4fef03e7f46dcf58680c688438e876a49
Dhruv Marwaha <108002460+dhruv-marwaha-03@users.noreply.github.com>
2022-06-25 20:48:32 +0530

    Update main.dart

--- lib/Dhruv/main.dart
+++ lib/Dhruv/main.dart
@@ -22,6 +22,7 @@ void main() {
                   width: 300,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderRadius: BorderRadius.only(bottomLeft:Radius.circular(10),bottomRight: Radius.circular(10)),
                       image: DecorationImage(
                           image:NetworkImage('https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt0c118364c6320f60/62a289d3891af05acaff06b1/Pearl_Gallery_01.jpg?auto=webp&width=915')
                       )
