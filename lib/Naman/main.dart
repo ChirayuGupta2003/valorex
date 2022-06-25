8a3c7434bb45083e34277371b7684cc4a2c86898
Nandika Chhuneja <chhuneja.nandika@gmail.com>
2022-06-25 14:30:56 +0530

    Update main.dart

--- lib/Naman/main.dart
+++ lib/Naman/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'quote.dart';
+import 'quote.da'
 
 void main() => runApp(MaterialApp(
       title: "Valorex",
