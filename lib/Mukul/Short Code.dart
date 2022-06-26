860f7300b4f4131ba27eb7e5c3a483d3578cf720
ChirayuGupta <87424491+ChirayuGupta2003@users.noreply.github.com>
2022-06-27 01:42:00 +0530

    updated guns page

--- lib/Mukul/Short Code.dart
+++ lib/Mukul/Short Code.dart
@@ -70,75 +70,81 @@ class _HomeState extends State<Home> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          InkWell(
-            onTap: () {},
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                      'https://scontent.fluh2-1.fna.fbcdn.net/v/t1.6435-9/87142187_213455970056218_2898787493811847168_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=U30Hpjy751cAX_k7OrJ&_nc_ht=scontent.fluh2-1.fna&oh=00_AT-4wMfV2nnUPdBfC6THcTogxBe3uxMrlWYLXlXuIdhy4Q&oe=62DE0CC7'),
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              height: 225,
-              width: 175,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Image(
-                    image: NetworkImage(character.url),
+          Expanded(
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                margin: EdgeInsets.fromLTRB(5, 2.5, 2.5, 2.5),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://scontent.fluh2-1.fna.fbcdn.net/v/t1.6435-9/87142187_213455970056218_2898787493811847168_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=U30Hpjy751cAX_k7OrJ&_nc_ht=scontent.fluh2-1.fna&oh=00_AT-4wMfV2nnUPdBfC6THcTogxBe3uxMrlWYLXlXuIdhy4Q&oe=62DE0CC7'),
+                    fit: BoxFit.cover,
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        character.name,
-                        style: TextStyle(
-                          fontFamily: "Valorant1",
-                          fontSize: 15,
-                          color: Colors.white,
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                height: 225,
+                width: 175,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image(
+                      image: NetworkImage(character.url),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          character.name,
+                          style: TextStyle(
+                            fontFamily: "Valorant1",
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
-          InkWell(
-            onTap: () {},
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                      'https://scontent.fluh2-1.fna.fbcdn.net/v/t1.6435-9/87142187_213455970056218_2898787493811847168_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=U30Hpjy751cAX_k7OrJ&_nc_ht=scontent.fluh2-1.fna&oh=00_AT-4wMfV2nnUPdBfC6THcTogxBe3uxMrlWYLXlXuIdhy4Q&oe=62DE0CC7'),
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              height: 225,
-              width: 175,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Image(
-                    image: NetworkImage(character.url1),
+          Expanded(
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                margin: EdgeInsets.fromLTRB(2.5, 2.5, 5, 2.5),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://scontent.fluh2-1.fna.fbcdn.net/v/t1.6435-9/87142187_213455970056218_2898787493811847168_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=U30Hpjy751cAX_k7OrJ&_nc_ht=scontent.fluh2-1.fna&oh=00_AT-4wMfV2nnUPdBfC6THcTogxBe3uxMrlWYLXlXuIdhy4Q&oe=62DE0CC7'),
+                    fit: BoxFit.cover,
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        character.name1,
-                        style: TextStyle(
-                          fontFamily: "Valorant1",
-                          fontSize: 15,
-                          color: Colors.white,
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                height: 225,
+                width: 175,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image(
+                      image: NetworkImage(character.url1),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          character.name1,
+                          style: TextStyle(
+                            fontFamily: "Valorant1",
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -150,94 +156,95 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      backgroundColor: Color(0xFF172535),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 130,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                      'https://scontent.fluh2-1.fna.fbcdn.net/v/t1.6435-9/87142187_213455970056218_2898787493811847168_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=U30Hpjy751cAX_k7OrJ&_nc_ht=scontent.fluh2-1.fna&oh=00_AT-4wMfV2nnUPdBfC6THcTogxBe3uxMrlWYLXlXuIdhy4Q&oe=62DE0CC7'),
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    "AGENTS",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontFamily: "Valorant1",
-                      color: Colors.white,
-                    ),
+      child: Scaffold(
+        backgroundColor: Color(0xFF172535),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 130,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://scontent.fluh2-1.fna.fbcdn.net/v/t1.6435-9/87142187_213455970056218_2898787493811847168_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=U30Hpjy751cAX_k7OrJ&_nc_ht=scontent.fluh2-1.fna&oh=00_AT-4wMfV2nnUPdBfC6THcTogxBe3uxMrlWYLXlXuIdhy4Q&oe=62DE0CC7'),
+                    fit: BoxFit.cover,
                   ),
-                ],
-              ),
-            ),
-            Column(
-              children:
-                  character_info.map((e) => Character_Template(e)).toList(),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(4, 15, 0, 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onTap: (){},
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              'https://scontent.fluh2-1.fna.fbcdn.net/v/t1.6435-9/87142187_213455970056218_2898787493811847168_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=U30Hpjy751cAX_k7OrJ&_nc_ht=scontent.fluh2-1.fna&oh=00_AT-4wMfV2nnUPdBfC6THcTogxBe3uxMrlWYLXlXuIdhy4Q&oe=62DE0CC7'),
-                          fit: BoxFit.cover,
-                        ),
-                        borderRadius: BorderRadius.circular(15.0),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "AGENTS",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontFamily: "Valorant1",
+                        color: Colors.white,
                       ),
-                      height: 225,
-                      width: 175,
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          Image(
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children:
+                    character_info.map((e) => Character_Template(e)).toList(),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 15, 0, 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
                             image: NetworkImage(
-                                'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt516d37c6c84fcda0/625db737c9aa404b76ddd594/Fade_Key_Art_587x900_for_Website.png'),
-                            // height: 100,
-                            // fit: BoxFit.contain,
+                                'https://scontent.fluh2-1.fna.fbcdn.net/v/t1.6435-9/87142187_213455970056218_2898787493811847168_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=U30Hpjy751cAX_k7OrJ&_nc_ht=scontent.fluh2-1.fna&oh=00_AT-4wMfV2nnUPdBfC6THcTogxBe3uxMrlWYLXlXuIdhy4Q&oe=62DE0CC7'),
+                            fit: BoxFit.cover,
                           ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text(
-                                "FADE",
-                                style: TextStyle(
-                                  fontFamily: "Valorant1",
-                                  fontSize: 15,
-                                  color: Colors.white,
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        height: 225,
+                        width: 175,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Image(
+                              image: NetworkImage(
+                                  'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt516d37c6c84fcda0/625db737c9aa404b76ddd594/Fade_Key_Art_587x900_for_Website.png'),
+                              // height: 100,
+                              // fit: BoxFit.contain,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                  "FADE",
+                                  style: TextStyle(
+                                    fontFamily: "Valorant1",
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          )
-                        ],
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
