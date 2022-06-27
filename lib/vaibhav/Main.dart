094be39b569bd8d82c53aa32d24bedad9addea19
ChirayuGupta <87424491+ChirayuGupta2003@users.noreply.github.com>
2022-06-28 00:01:43 +0530

    code cleanup

--- lib/vaibhav/Main.dart
+++ lib/vaibhav/Main.dart
@@ -4,7 +4,7 @@ import 'package:valorex/Naman/main.dart';
 import 'package:valorex/vaibhav/loadingscreen.dart';
 import 'package:valorex/Mukul/main.dart';
 import 'package:valorex/Dhruv/maps.dart';
-import 'package:valorex/Nandika/new.dart';
+import 'package:valorex/Nandika/gunsList.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/loadingscreen',
