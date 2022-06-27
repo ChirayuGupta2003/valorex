89697d7e2ec9e373df0f59bd938caa1e4213b504
Vaibhavk0192 <vkapoor2_be21@thapar.edu>
2022-06-27 18:19:03 +0530

    issue solved

--- lib/Naman/main.dart
+++ lib/Naman/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'quote.da'
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
       title: "Valorex",
