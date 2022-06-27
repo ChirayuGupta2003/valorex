efc4fac2abb37a6a7d39b17107e4711f9cca32ed
ChirayuGupta <87424491+ChirayuGupta2003@users.noreply.github.com>
2022-06-27 23:26:21 +0530

    Update new.dart

--- lib/Nandika/new.dart
+++ lib/Nandika/new.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -88,78 +90,80 @@ class _ListViewHomePageState extends State<ListViewHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width * 0.6;
-    return Scaffold(
-      backgroundColor: Color(0xFF0f1923),
-      appBar: AppBar(
-        title: Center(
-          child: Container(
-            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-            child: Text(
-              "GUNS",
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Valorant1',
-                fontSize: 40,
+    // double width = MediaQuery.of(context).size.width * 0.6;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFF0f1923),
+        appBar: AppBar(
+          title: Center(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: Text(
+                "GUNS",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Valorant1',
+                  fontSize: 40,
+                ),
               ),
             ),
           ),
+          centerTitle: true,
+          backgroundColor: Color(0xFF0f1923),
         ),
-        centerTitle: true,
-        backgroundColor: Color(0xFF0f1923),
-      ),
-      body: SafeArea(
-        child: ListView.builder(
+        body: SafeArea(
+          child: ListView.builder(
             itemCount: imgList.length,
             itemBuilder: (context, index) {
               return GestureDetector(
+                onTap: () {},
+                child: InkWell(
                   onTap: () {},
-                  child: InkWell(
-                    onTap: () {},
-                    child: Card(
-                        color: Colors.redAccent,
-                        child: Row(
-                          children: <Widget>[
-                            Container(
-                              padding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
-                              margin: EdgeInsets.all(10.0),
-                              width: 110,
-                              height: 110,
-                              child: Image.asset(imgList[index]),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    titleList[index],
-                                    style: TextStyle(
-                                      fontSize: 28,
-                                      color: Color(0xFF0f1923),
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Valorant1',
-                                    ),
-                                  ),
-                                  SizedBox(height: 10),
-                                  Container(
-                                    width: width,
-                                    child: Text(
-                                      descList[index],
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        color: Colors.white,
-                                        fontFamily: 'Valorant1',
-                                      ),
-                                    ),
-                                  )
-                                ],
+                  child: Card(
+                    color: Colors.redAccent,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+                          margin: EdgeInsets.all(10.0),
+                          width: 110,
+                          height: 110,
+                          child: Image.asset(imgList[index]),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                titleList[index],
+                                style: TextStyle(
+                                  fontSize: 27,
+                                  color: Color(0xFF0f1923),
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Valorant1',
+                                ),
                               ),
-                            )
-                          ],
-                        )),
-                  ));
-            }),
+                              SizedBox(height: 10),
+                              Text(
+                                descList[index],
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontFamily: 'Valorant1',
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
