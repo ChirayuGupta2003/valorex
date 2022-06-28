8a272373ac6c4305c337d1fb9ad5e6f3212f2df7
Dhruv Marwaha <108002460+dhruv-marwaha-03@users.noreply.github.com>
2022-06-28 17:51:37 +0530

    new files

--- lib/Dhruv/Split.dart
+++ lib/Dhruv/Split.dart
@@ -26,18 +26,18 @@ class Test extends StatelessWidget {
                 decoration:BoxDecoration(
                   borderRadius: BorderRadius.circular(24),
                   image: DecorationImage(
-                      fit: BoxFit.fitWidth,
-                      image: NetworkImage('https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt0c118364c6320f60/62a289d3891af05acaff06b1/Pearl_Gallery_01.jpg?auto=webp&width=915')
+                    fit: BoxFit.fitWidth,
+                    image: NetworkImage('https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt643d33e2defb855c/5eabe9fe248a28605479c547/split1.jpg?auto=webp&width=915'),
                   ),
                 ),
                 child: Align(
 
                     alignment: Alignment.bottomCenter,
-                    child:Text('PEARL',
+                    child:Text('SPLIT',
                       textAlign: TextAlign.center,
 
                       style:TextStyle(
-                          color: Colors.white70,
+                          color: Colors.white,
                           fontSize: 50,
                           fontWeight:FontWeight.bold,
                           fontFamily: 'Valorant1'
@@ -57,12 +57,13 @@ class Test extends StatelessWidget {
             child:
             Container(
               child:Text(
-                'Attackers push down into the Defenders on this '
-                    'two-site map set in a vibrant,underwater city. '
-                    'Pearl is a geo-driven map with no mechanics.'
-                    'Take the fight through a compact mid or the'
-                    'longer range wings in our first map set in Omega '
-                    'Earth',
+                'If you want to go far, you’ll have to go up. '
+                    'A pair of sites split by an elevated center '
+                    'allows for rapid movement using two '
+                    'rope ascenders. Each site is built with a '
+                    'looming tower vital for control. '
+                    'Remember to watch above before it all '
+                    'blows sky-high.',
                 style: TextStyle(
                   color: Colors.white70,
                   fontSize: 20,
@@ -84,8 +85,9 @@ class Test extends StatelessWidget {
 
                 image: DecorationImage(
                   fit: BoxFit.fitWidth,
-                  image:NetworkImage('https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt9011a376cfbacccf/62a28a2334f22956975fa6d1/Pearl_Map_Website_641x641.png?auto=webp&width=515'),
-                ),),
+                  image:NetworkImage('https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt2caea7a88362d6aa/5ecd64b0817e574fa1dcc162/split-minimap-2.png?auto=webp&width=515'),
+                ),
+              ),
             ),)
 
 
